cfeb57b29a3c04a7e92c12ea9b1db7bafa91c409
MichalWitalis <michal.witalis@student.wsb.edu.pl>
2026-04-16 21:27:11 +0200

    Add files via upload

---
+++ python/z2_konwerter.py
@@ -0,0 +1,18 @@
+﻿print("Konwerter temperatur")
+print("C - Celsjusz -> Fahrenheit")
+print("F - Fahrenheit -> Celsjusz")
+
+opcja = input("Wybierz opcję (C/F): ")
+
+if opcja == 'C':
+    c = float(input("Podaj temperaturę w stopniach Celsjusza: "))
+    f = c * 1.8 + 32
+    print(f"{c} °C = {f} °F")
+
+elif opcja == 'F':
+    f = float(input("Podaj temperaturę w stopniach Fahrenheita: "))
+    c = (f - 32) / 1.8
+    print(f"{f} °F = {c} °C")
+
+else:
+    print("Błędna opcja")
